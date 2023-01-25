c84bae8eaeb2f9839bb138774aa95e16c962460c
mjnslf <frecklessmadina@gmail.com>
2023-01-25 21:30:31 +0600

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let s = '';
+    let reversedStr = '';
+
+    if (n < 0){
+        n *= (-1);
+    }
+
+    s = n.toString();
+
+    for (let i = s.length - 1; i >= 0; i--) {
+    reversedStr += s[i];
+    }
+    return (Number(reversedStr));
 }
